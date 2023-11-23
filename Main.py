450e4901c2ece15d7e374c6f4241df35d2def1fe
LucasFinney <70116506+LucasFinney@users.noreply.github.com>
2023-11-23 13:48:55 -0400

    Added files and an example data file

---
+++ Main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 13:06:58 2023
+
+@author: Lucas Finney
+"""
+import csv
+import os
+
+path = input("Enter file path (or just the name if it's in this folder)")
+numCols = input("Enter the desired number of columns")
+
+# Open the file
+with open(path,'r') as f:
+    data = [line.strip() for line in f] #Iterate over the lines, creating a new list element for each
+    
+# Group every pair
+data = zip(*[iter(data)]*3)
+
+# Create the CSV file
+with open('output.csv','w', newline = '') as f:
+    writer = csv.writer(f)
+    for row in data:
+        writer.writerow(row) # Writing out the pairs in our data as a row of two columns
+
+os.system('cls')
+
+print("Result")
+print("-------")
+# Print out the "CSV" version to check if it worked
+with open('output.csv','r') as f:
+    lines = [line.strip() for line in f]
+    for line in lines:
+        print(line)
+
+print("=====")
+input("Press enter to exit")
